78c36b70fa93a5830b4a3c90bb4ca97575dea3ec
Nick Waddoups <nick.waddoups@usu.edu>
2025-01-29 15:52:32 -0700

    Added fmt library

--- Homework/2/main.cpp
+++ Homework/2/main.cpp
@@ -1,6 +1,17 @@
+#include "fmt/core.h"
 #include <thread>
+#include <span>
+#include <fmt/format.h>
 
-auto main() -> int 
+auto main(int argc, char **argv) -> int 
 {
+    constexpr static auto EXPECTED_ARGC = 5U;
+    const std::span<char *> args(argv, static_cast<std::size_t>(argc));
+
+    if (args.size() != EXPECTED_ARGC) {
+        fmt::println("Usage: <number of threads> <bin count> <min meas> <max meas> <data count>");
+        return EXIT_FAILURE;
+    }
+
     return 0;
 }
