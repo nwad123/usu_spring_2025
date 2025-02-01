e525ea99c865f2ce16bcbb005e76e3020b480bc6
Nick Waddoups <nick.waddoups@usu.edu>
2025-01-31 23:59:53 -0700

    Fix - Made sure all tests run

--- Homework/2/auto.cpp
+++ Homework/2/auto.cpp
@@ -12,7 +12,7 @@ using namespace hpc;
 auto main() -> int
 {
     static constexpr std::array<size_t, 6> THREADS = { 1, 2, 4, 8, 16, 32 };
-    static constexpr std::array<size_t, 6> SIZES = { 1'000'000, 10'000'000, 100'000'000, 1'000'000'000 };
+    static constexpr std::array<size_t, 6> SIZES = { 1'000'000, 10'000'000, 100'000'000, 1'000'000'000, 2'000'000'000, 4'000'000'000 };
     static constexpr std::pair<fp, fp> RANGE = { 0.0, 5.0 };
     static constexpr auto BINS = 5;
 
@@ -23,7 +23,6 @@ auto main() -> int
     // handrolled cartesian product
     for (const auto threads : THREADS) {
         for (const auto size : SIZES) {
-            fmt::println("LOG: testing {} threads on {} size", threads, size);
             Config config(threads, BINS, RANGE.first, RANGE.second, size);
             auto dataset = make_dataset(config);
 
@@ -34,7 +33,6 @@ auto main() -> int
     }
 
     // output all of the results
-    fmt::println("RESULTS:");
     fmt::println("Name,Threads,Elements,Time(ms)");
     for (const auto &result : results) {
         fmt::println("{},{},{},{}", result.name, result.config.threads, result.config.size, result.time_ms);
