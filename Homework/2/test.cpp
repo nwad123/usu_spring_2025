ee94bca3d1c7ce294ae0665fe904be1d6e9546c3
Nick Waddoups <nick.waddoups@usu.edu>
2025-02-03 18:07:53 -0700

    Expanded test framework

--- Homework/2/test.cpp
+++ Homework/2/test.cpp
@@ -12,24 +12,37 @@ using namespace hpc;
 
 auto main() -> int
 {
+    // constants
     static constexpr auto THREADS = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    static constexpr size_t SIZE = 10'000'000;
+    static constexpr auto SIZES = { 10'000'000, 100'000'000, 1'000'000'000 };
     static constexpr std::pair<fp, fp> RANGE = { 0.0, 5.0 };
     static constexpr auto BINS = 5;
 
-    auto dataset = make_dataset(RANGE.first, RANGE.second, SIZE, std::mt19937{ 100 });
+    // id for tests
+    size_t test_id{ 0 };
 
-    for (const auto THREAD : THREADS) {
-        const Config config(static_cast<size_t>(THREAD), BINS, RANGE.first, RANGE.second, dataset.size());
+    // handrolled cartesian product for different configurations
+    for (const auto SIZE : SIZES) {
+        const auto size = static_cast<size_t>(SIZE);
 
-        const auto solver_tester = SolverTester(config, dataset);
+        fmt::println("Generating dataset with {} values", size);
+        auto dataset = make_dataset(RANGE.first, RANGE.second, size, std::mt19937{ 100 });
 
-        if (not solver_tester(Serial{}, Parallel{}, Tree{})) {
-            fmt::println("Failed with parameters:");
-            config.print();
-            break;
-        } else {
-            fmt::println("Passed");
+        for (const auto THREAD : THREADS) {
+            const auto thread = static_cast<size_t>(THREAD);
+
+            const Config config(thread, BINS, RANGE.first, RANGE.second, dataset.size());
+            const auto solver_tester = SolverTester(config, dataset);
+
+            if (not solver_tester(Serial{}, Parallel{}, Tree{})) {
+                fmt::println("Failed test {} with parameters:", test_id);
+                config.print();
+                break;
+            } else {
+                fmt::println("Passed test {} with {} threads", test_id, thread);
+            }
+
+            test_id++;
         }
     }
 }
